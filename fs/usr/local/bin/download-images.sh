329cef609d6834228ec70a4320ab3a1633e2357c
Martins Balodis <martins256@gmail.com>
2017-07-13 15:15:44 +0300

    fix image service wasn't working

--- fs/usr/local/bin/download-images.sh
+++ fs/usr/local/bin/download-images.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 mkdir -p /root/Pictures
-for i in {1..10}; do wget https://unsplash.it/800/600/?random -O "/root/Pictures/$i.jpg"; done
+for i in {1..10}; do wget http://loremflickr.com/800/600 -O "/root/Pictures/$i.jpg"; done
